f274d261a07a4d24262611e74c6e24c1190318d4
tomoponzoo <tomoki.koga@plaid.co.jp>
2019-09-06 17:30:14 +0900

    Update: example app

--- example/App.js
+++ example/App.js
@@ -7,61 +7,71 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, TextInput, Button, View, SafeAreaView, Linking, Alert} from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView, TextInput, Button, SafeAreaView, Linking, Alert} from 'react-native';
+import {KarteTracker, KarteTrackerJsUtil, KarteInAppMessagingManager} from 'react-native-karte-tracker';
 
-import KarteTracker from 'react-native-karte-tracker';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
+type Props = {};
+export default class App extends Component<Props> {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       view: '',
       user_id: '',
       custom: ''
-    };
+    }
   }
-
-  componentDidMount() {
-    Linking.addEventListener('url', this._handleOpenURL);
-    Linking.getInitialURL().then((url) => {
-      if (url) {
-        console.log(`Linking.getInitialURL is ${url}`);
-        this._handleOpenURL({url});
-      }
-    }).catch(err => console.error('An error occurred', err));
-  }
-
-  componentWillUnmount() {
-    Linking.removeEventListener('url', this._handleOpenURL);
-  }
-
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <View style={styles.subContainer}>
-          <Text style={styles.header}>View event</Text>
-          <TextInput onChangeText={(view) => this.setState({view})} placeholder="View event name" value={this.state.view} />
-          <Button onPress={() => this._onPressSendViewEventButton()} title="Send" />
-        </View>
-        <View style={styles.subContainer}>
-          <Text style={styles.header}>Identify event</Text>
-          <TextInput onChangeText={(user_id) => this.setState({user_id})} placeholder="User ID" value={this.state.user_id} />
-          <Button onPress={() => this._onPressSendIdentifyEventButton()} title="Send" />
-        </View>
-        <View style={styles.subContainer}>
-          <Text style={styles.header}>Custom event</Text>
-          <TextInput onChangeText={(custom) => this.setState({custom})} placeholder="Custom event name" value={this.state.custom} />
-          <Button onPress={() => this._onPressSendCustomEventButton()} title="Send" />
-        </View>
+        <ScrollView>
+          <View style={styles.subContainer}>
+            <Text style={styles.header}>View event</Text>
+            <TextInput onChangeText={(view) => this.setState({view})} placeholder="View event name" value={this.state.view} />
+            <Button onPress={() => this._onPressSendViewEventButton()} title="Send" />
+          </View>
+          <View style={styles.subContainer}>
+            <Text style={styles.header}>Identify event</Text>
+            <TextInput onChangeText={(user_id) => this.setState({user_id})} placeholder="User ID" value={this.state.user_id} />
+            <Button onPress={() => this._onPressSendIdentifyEventButton()} title="Send" />
+          </View>
+          <View style={styles.subContainer}>
+            <Text style={styles.header}>Custom event</Text>
+            <TextInput onChangeText={(custom) => this.setState({custom})} placeholder="Custom event name" value={this.state.custom} />
+            <Button onPress={() => this._onPressSendCustomEventButton()} title="Send" />
+          </View>
+          <View style={styles.subContainer}>
+            <Text style={styles.header}>In-app messaging</Text>
+            <Button onPress={() => this._onPressIsPresentingButton()} title="Is presenting" />
+            <Button onPress={() => this._onPressDismissButton()} title="Dismiss" />
+            <Button onPress={() => this._onPressSuppressButton()} title="Suppress" />
+            <Button onPress={() => this._onPressUnsuppressButton()} title="Unsuppress" />
+          </View>
+          <View style={styles.subContainer}>
+            <Text style={styles.header}>Opt-in / out</Text>
+            <Button onPress={() => this._onPressOptInButton()} title="Opt-in" />
+            <Button onPress={() => this._onPressOptOutButton()} title="Opt-out" />
+          </View>
+          <View style={styles.subContainer}>
+            <Text style={styles.header}>Renew visitor id</Text>
+            <Button onPress={() => this._onPressRenewVisitorIdButton()} title="Renew" />
+          </View>
+        </ScrollView>
       </SafeAreaView>
-    );
+    )
+  }
+
+  componentDidMount() {
+    Linking.addEventListener('url', this._handleOpenURL)
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+        console.log(`Linking.getInitialURL is ${url}`)
+        this._handleOpenURL({url})
+      }
+    }).catch(err => console.error('An error occurred', err))
+  }
+
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this._handleOpenURL)
   }
 
   _handleOpenURL(event) {
@@ -90,8 +100,36 @@ export default class App extends Component {
       KarteTracker.track(this.state.custom);
     }
   }
-}
 
+  _onPressIsPresentingButton() {
+    console.dir(KarteInAppMessagingManager, {depth: 10})
+    KarteInAppMessagingManager.isPresenting((res) => {
+      Alert.alert('Is presenting', '' + res, [{
+        text: 'OK',
+        onPress: () => {}
+      }])
+    })
+  }
+
+  _onPressDismissButton() {
+    KarteInAppMessagingManager.dismiss()
+  }
+  _onPressSuppressButton() {
+    KarteInAppMessagingManager.suppress()
+  }
+  _onPressUnsuppressButton() {
+    KarteInAppMessagingManager.unsuppress()
+  }
+  _onPressOptInButton() {
+    KarteTracker.optIn()
+  }
+  _onPressOptOutButton() {
+    KarteTracker.optOut()
+  }
+  _onPressRenewVisitorIdButton() {
+    KarteTracker.renewVisitorId()
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -120,4 +158,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
